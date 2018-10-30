b605c6c6877bcd3e744d32c39bb8af922bcb2ec2
Didi Medina <hi@didimedina.com>
2018-10-30 12:06:07 +0200

    console log added for testing

--- esi2018.js
+++ esi2018.js
@@ -1,3 +1,4 @@
 alert('script is working!');
 //alert(document.getElementById("test-element").style.backgroundColor)
 document.body.style.backgroundColor = "red";
+console.log(document.getElementId('testElement').style.backgroundColor);
