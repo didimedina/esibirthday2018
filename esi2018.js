ce3938b7cecefde0fbfbf8458025be274c9268ef
Didi Medina <hi@didimedina.com>
2018-10-30 12:07:09 +0200

    fixed typo

--- esi2018.js
+++ esi2018.js
@@ -1,4 +1,4 @@
 alert('script is working!');
 //alert(document.getElementById("test-element").style.backgroundColor)
 document.body.style.backgroundColor = "red";
-console.log(document.getElementId('testElement').style.backgroundColor);
+console.log(document.getElementById('testElement').style.backgroundColor);
