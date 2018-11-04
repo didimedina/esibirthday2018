ef42eb07b50ab124d4fdf1ac321f94a5c8e3ce43
Didi Medina <hi@didimedina.com>
2018-11-04 21:10:24 +0200

    new update

--- esi2018.js
+++ esi2018.js
@@ -1,4 +1,46 @@
 alert('script is working!');
-//alert(document.getElementById("test-element").style.backgroundColor)
+//document.getElementById("testElement").style.backgroundColor = red;
 document.body.style.backgroundColor = "red";
-console.log(document.getElementById('testElement').style.backgroundColor);
+//console.log(document.getElementById('testElement').style.backgroundColor);
+
+
+/*
+var randomNumber = getRandomNumber(10);
+var guess;
+var guessCount = 0;
+var correctGuess = false;
+
+function getRandomNumber( upper ) {
+  var num = Math.floor(Math.random() * upper) + 1;
+  return num;
+}
+
+/* do {
+  guess = prompt('I am thinking of a number between 1 and 10. What is it?');
+  guessCount += 1;
+  if (parseInt(guess) === randomNumber) {
+    correctGuess = true;
+  }
+} while ( ! correctGuess )
+
+document.write('<h1>You guessed the number!</h1>');
+document.write('It took you ' + guessCount + ' tries to guess the number ' + randomNumber);
+
+
+while (guessCounter < 10) {
+  guess = prompt('I am thinking of a number between 1 and 10. What is it?');
+  guessCount += 1;
+  if ( parseInt(guess) === randomNumber) {
+    correctGuess = true;
+    break;
+  }
+}
+
+if (correctGuess) {
+  document.write('<h1>You guessed the number!</h1>');
+  document.write('It took you ' + guessCount + ' tries to guess the number ' + randomNumber);
+} else {
+  document.write('<h1> Sorry. You did not guess the number.</h1>');
+}
+
+*/
