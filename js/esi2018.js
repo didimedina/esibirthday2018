39e2b5a39098d3a6431aa3c222a36929ce28944b
Didi Medina <hi@didimedina.com>
2018-11-28 12:16:24 +0200

    upload all

---
+++ js/esi2018.js
@@ -0,0 +1,10 @@
+const body = document.getElementById('body');
+const question = document.getElementById('question');
+
+const answerField = document.getElementById('answer');
+
+answerField.addEventListener('click', () => {
+    answerField.placeholder = "Give it your best shot!";
+})
+
+
