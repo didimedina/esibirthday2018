12e34f94360977e83bc02d47b4541a30f9d6ad83
Didi Medina <hi@didimedina.com>
2018-11-29 08:22:38 +0200

    add question

--- esi2018.js
+++ esi2018.js
@@ -1,10 +1,33 @@
-const body = document.getElementById('body');
-const question = document.getElementById('question');
+// const body = document.getElementById('body');
+// const question = document.getElementById('question');
 
-const answerField = document.getElementById('answer');
+// const answerField = document.getElementById('answer');
 
-answerField.addEventListener('click', () => {
-    answerField.placeholder = "Give it your best shot!";
-});
+// answerField.addEventListener('click', () => {
+//     answerField.placeholder = "Give it your best shot!";
+// });
 
+//––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––\\
+
+// Variables
+const question = document.getElementById("question");
+
+
+//Placing the first question...
+question.innerHTML = "What was the resturuant we ate at after I landed in Israel for the first time?";
+
+
+// const myButton = document.getElementById("myButton");
+// const myInput = document.getElementById("myInput");
+
+// myButton.addEventListener("click", () => {
+//     let answer = myInput.value;
+//     if (answer === "red") {
+//         myHeading.style.color = "green";
+//         myHeading.innerHTML = "You got it!";
+//     } else {
+//         myHeading.style.color = "red";
+//         myHeading.innerHTML = "Try again.";
+//     }
+// });
 
