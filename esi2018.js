c729881bb72676f830e3cdb401548db627347e87
Didi Medina <hi@didimedina.com>
2018-11-29 08:37:48 +0200

    test

--- esi2018.js
+++ esi2018.js
@@ -12,11 +12,16 @@
 // Variables
 const question = document.getElementById("question");
 const body = document.getElementsByName('body');
+const answerField = document.getElementById('answer');
 
 
 //Placing the first question...
 body.style.color = "black";
 question.innerHTML = "What was the resturuant we ate at after I landed in Israel for the first time?";
+
+answerField.addEventListener('click', () => {
+    answerField.placeholder = "Give it your best shot!";
+});
 
 
 // const myButton = document.getElementById("myButton");
