cc2cb37e0605fee398b911d057ca6895a83e1e4c
Didi Medina <hi@didimedina.com>
2018-12-01 14:22:16 +0200

    removed extra code

--- esi2018.js
+++ esi2018.js
@@ -1,14 +1,3 @@
-// const body = document.getElementById('body');
-// const question = document.getElementById('question');
-
-// const answerField = document.getElementById('answer');
-
-// answerField.addEventListener('click', () => {
-//     answerField.placeholder = "Give it your best shot!";
-// });
-
-//––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––\\
-
 // Variables
 const heading = document.getElementById("heading");
 const input = document.getElementById('input');
@@ -33,7 +22,6 @@ const questions = [
     }
 ];
 
-//Placing the first question staticly...
 function updateQuestion() {
     heading.innerHTML = questions[currentQuestion].question;
     input.placeholder = "Type your answer...";
@@ -41,8 +29,11 @@ function updateQuestion() {
     input.value = "";
 };
 
+//Placing the first question staticly...
 updateQuestion();
 
+
+// Validating answers once submitted. 
 submitBtn.addEventListener('click', () => {
     let submittedAnswer = input.value.toLowercase();
     if (questions[currentQuestion].correctAnswers.indexOf(submittedAnswer) > -1) {
@@ -56,49 +47,4 @@ submitBtn.addEventListener('click', () => {
 
 });
 
-
-
-// right now the for loop causes the question to be propegated automatically.
-// however, you need another for loop to validate answers which isn't working at the moment.
-// it might make sense to make the validating the answe step its own function seperately,
-// but if it lives locally than that might defeat the purpose.
-
-// if (score < 3){  
-//     for (i in questions) {
-//         heading.innerHTML = questions[i].question
-//         submitBtn.addEventListener("click", () => {
-//             let submittedAnswer = input.value;
-//             for (j in questions[i].correctAnswers) {
-//                 if (submittedAnswer === questions[i].correctAnswers[j]) {
-//                     heading.style.color = "green";
-//                 } else {
-//                     heading.style.color = "red";
-//                     input.value = "";
-//                     input.placeholder = "Nope, try again!";
-//                 }
-//                 // console.log(questions[i], ':', questions[i].correctAnswers[j]);
-//             };
-//         });  
-//     };
-// };
-
-//Placing the first question via objects...
-// heading.innerHTML = questions;
-
-
-input.addEventListener('click', () => {
-    input.placeholder = "Give it your best shot!";
-});
-
-
-// submitBtn.addEventListener("click", () => {
-//     let submittedAnswer = input.value;
-//     if (submittedAnswer === questions[0].correctAnswers[0]) {
-//         heading.style.color = "green";
-//     } else {
-//         heading.style.color = "red";
-//         input.value = "";
-//         input.placeholder = "Nope, try again!";
-//     }
-// });
 
