3a3122b3a4056fb0ff7de0bdc57e66bb55702f26
Didi Medina <hi@didimedina.com>
2018-12-01 14:09:00 +0200

    works for the first two not the third

--- esi2018.js
+++ esi2018.js
@@ -13,7 +13,7 @@
 const heading = document.getElementById("heading");
 const input = document.getElementById('input');
 const submitBtn = document.getElementById('submit-btn');
-let score = 0;
+let currentQuestion = 0;
 
 // Question objects...
 const questions = [
@@ -23,37 +23,64 @@ const questions = [
     },
 
     {
-        question: "What appointment did I cancle in budapest??",
+        question: "What appointment did I cancle in budapest?",
         correctAnswers: ["haircut", "barber", "barber shop", "hair salon"]
+    },
+
+    {
+        question: "What nickname did I give you?",
+        correctAnswers: ["chuchu", "choochoo", "chooch", "chu chu", 'choochoo']
     }
 ];
 
 //Placing the first question staticly...
-// heading.innerHTML = questions[0].question
+function updateQuestion() {
+    heading.innerHTML = questions[currentQuestion].question;
+    input.placeholder = "Type your answer...";
+    heading.style.color = "";
+    input.value = "";
+};
+
+updateQuestion();
+
+submitBtn.addEventListener('click', () => {
+    let submittedAnswer = input.value;
+    if (questions[currentQuestion].correctAnswers.indexOf(submittedAnswer) > -1) {
+        currentQuestion =+ 1;
+        updateQuestion();
+    } else {
+        heading.style.color = "red";
+        input.value = "";
+        input.placeholder = "Nope, try again!";
+    };
+
+});
+
+
 
 // right now the for loop causes the question to be propegated automatically.
 // however, you need another for loop to validate answers which isn't working at the moment.
 // it might make sense to make the validating the answe step its own function seperately,
 // but if it lives locally than that might defeat the purpose.
 
-if (score < 3){  
-    for (i in questions) {
-        heading.innerHTML = questions[i].question
-        submitBtn.addEventListener("click", () => {
-            let submittedAnswer = input.value;
-            for (j in questions[i].correctAnswers) {
-                if (submittedAnswer === questions[i].correctAnswers[j]) {
-                    heading.style.color = "green";
-                } else {
-                    heading.style.color = "red";
-                    input.value = "";
-                    input.placeholder = "Nope, try again!";
-                }
-                // console.log(questions[i], ':', questions[i].correctAnswers[j]);
-            };
-        });  
-    };
-};
+// if (score < 3){  
+//     for (i in questions) {
+//         heading.innerHTML = questions[i].question
+//         submitBtn.addEventListener("click", () => {
+//             let submittedAnswer = input.value;
+//             for (j in questions[i].correctAnswers) {
+//                 if (submittedAnswer === questions[i].correctAnswers[j]) {
+//                     heading.style.color = "green";
+//                 } else {
+//                     heading.style.color = "red";
+//                     input.value = "";
+//                     input.placeholder = "Nope, try again!";
+//                 }
+//                 // console.log(questions[i], ':', questions[i].correctAnswers[j]);
+//             };
+//         });  
+//     };
+// };
 
 //Placing the first question via objects...
 // heading.innerHTML = questions;
