84c4474e0da5ac69c94461dd404ceb1aefb9eb91
Didi Medina <hi@didimedina.com>
2019-01-06 10:07:40 +0200

    added new js file for airtable api

---
+++ airtableAPI.js
@@ -0,0 +1,9 @@
+// Variables
+const quiz = document.getElementById("quiz");
+const birthdayLetter = document.getElementById("birthday-letter");
+const heading = document.getElementById("heading");
+const input = document.getElementById('input-answer');
+const submitBtn = document.getElementById('btn-submit');
+let score = 0;
+let currentQuestion = 0;
+let submittedAnswer = "";
