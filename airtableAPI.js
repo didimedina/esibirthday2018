eb643af74b9db7c3329bc5254c3c6d2144e683fd
Didi Medina <hi@didimedina.com>
2019-01-10 21:06:01 +0200

    code not working with fetch api

--- airtableAPI.js
+++ airtableAPI.js
@@ -1,44 +1,56 @@
 // Variables
-// const quiz = document.getElementById("quiz");
-// const birthdayLetter = document.getElementById("birthday-letter");
-// const heading = document.getElementById("heading");
-// const input = document.getElementById('input-answer');
-// const submitBtn = document.getElementById('btn-submit');
-// let score = 0;
-// let currentQuestion = 0;
-// let submittedAnswer = "";
+const quiz = document.getElementById("quiz");
+const birthdayLetter = document.getElementById("birthday-letter");
+const heading = document.getElementById("heading");
+const input = document.getElementById('input-answer');
+const submitBtn = document.getElementById('btn-submit');
+let score = 0;
+let currentQuestion = 0;
+let submittedAnswer = "";
 
-let output = document.getElementById('output').innerHTML;
+// Fetch Data From AirTable
 
-function fetchQuestions() {
-    fetch('https://api.airtable.com/v0/apped63ewykkw1H26/Questions?api_key=keyfWe1gli5Iryn4I')
-        .then((res) => res.json())
-        .then((jsonData) => {
-            console.log(jsonData);
-            jsonData = jsonData.records;
-            console.log(jsonData)
-            // jsonData = Object.entries(jsonData.records);
-            let output = '<h2>Questions</h2>'
+fetch('https://api.airtable.com/v0/apped63ewykkw1H26/Questions?api_key=keyfWe1gli5Iryn4I')
+    .then(res => res.json())
+    .then(json => populateQuiz(json.records))
 
-            jsonData.forEach(function (record) {
-                console.log(record.fields.Question);
-                output += `
-            <ul>
-                <li>Question: ${record.fields.Question}</li>
-            </ul>
-            `;
-            });
-
-            document.getElementById('output').innerHTML = output;
-            // console.log(jsonData);
-        })
-
+// Populate Quiz
+function populateQuiz(data){
+    if (score < data.length) {
+        heading.innerHTML = data[currentQuestion].fields.Question;
+        input.placeholder = "Type your answer...";
+        heading.style.color = "";
+        input.value = "";
+    } else {
+        //show birthday letter. 
+        quiz.style.display = "none";
+        birthdayLetter.style.display = "flex";
+    };
 }
 
-fetchQuestions();
+submitBtn.addEventListener('click', () => {
+    currentQuestion++;
+    score++;
+    populateQuiz();
+
+
+    // input.autofocus = true; trying to get the input to autofocus each time but its not working... 
+    // submittedAnswer = input.value.toLowerCase();
+    // if (questions[currentQuestion].correctAnswers.indexOf(submittedAnswer) > -1) {
+    //     currentQuestion++;
+    //     score++;
+    //     populateQuiz();
+    // } else {
+    //     heading.style.color = "red";
+    //     input.value = "";
+    //     input.placeholder = "Nope, try again!";
+    // };
+});
 
 // function updateQuestion() {
 //     if (score < questions.length) {
+        
+
 //         heading.innerHTML = questions[currentQuestion].question;
 //         input.placeholder = "Type your answer...";
 //         heading.style.color = "";
@@ -51,4 +63,50 @@ fetchQuestions();
 //         // heading.innerHTML = 'happy birthday!';
 //     };
 // };
+
+// async function getQuestions() {
+//     let res = await fetch(
+//         "https://api.airtable.com/v0/apped63ewykkw1H26/Questions?api_key=keyfWe1gli5Iryn4I"
+//     );
+//     let json = await res.json();
+//     json = json.records;
+//     return json;
+// }
+
+// console.log(getQuestions());
+
+// function populateQuiz() {
+//     console.log("populate quiz");
+//     for (let question of listOfQuestions.records) {
+//         console.log(question);
+//     }
+// }
+
+// let listOfQuestions;
+// // time 0
+// getQuestions().then(data => {
+//     console.log(`Returned data:`);
+//     listOfQuestions = data;
+//     console.log(listOfQuestions); // returning undefined.
+//     // time 1
+//     populateQuiz(listOfQuestions);
+// });
+
+// populateQuiz();
+// time 0
+
+// async function getLotsOfQuestions() {
+//   let promiseList = [];
+//   for (let i = 0; i < 10; i++) {
+//     promiseList.push(getQuestion());
+//   }
+
+//   let questionList = await Promise.all(promiseList);
+
+//   console.log(questionList);
+// }
+
+// getLotsOfQuestions();
+
+
 
